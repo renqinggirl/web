00702fd0e0f81ebde32ca1e7827f91571ca0ab9e
renqinggirl <33271272+renqinggirl@users.noreply.github.com>
2017-11-05 13:14:55 +0800

    Update Demo.java

--- src/cn/itcast/demo/Demo.java
+++ src/cn/itcast/demo/Demo.java
@@ -8,8 +8,9 @@ public class Demo {
 	}
 	public static void main(String[] args) {
 		System.out.println("haha");
-    	System.out.println("gugu");
-        System.out.println("gaggaa");
+    	        System.out.println("gugu");
+                System.out.println("gaggaa");
+		System.out.println("hhhggaa");
 	}
 
 }
