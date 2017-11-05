d59c743bf39aedd8d19a76f4ea7c8a512ff0e278
renqing <renqing0202@163.com>
2017-11-05 13:45:15 +0800

    1

--- src/cn/itcast/demo/Demo.java
+++ src/cn/itcast/demo/Demo.java
@@ -1,17 +1,19 @@
 package cn.itcast.demo;
 
 public class Demo {
-	int a=10;
+	int a = 10;
+
 	private void syso() {
 		// TODO Auto-generated method stub
 
 	}
+
 	public static void main(String[] args) {
 		System.out.println("haha");
 
-    	System.out.println("gugu");
-        System.out.println("gaggaa");
-        	System.out.println("hhhggaa");
+		System.out.println("gugu");
+		System.out.println("gaggaa");
+		System.out.println("hhhggaa");
 
 	}
 
